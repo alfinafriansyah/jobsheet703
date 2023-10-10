bcd70d8e27e5a1974556494585b2842302750455
Alfin Afriansyah <143513533+alfinafriansyah@users.noreply.github.com>
2023-10-10 15:16:47 +0700

    Add files via upload

---
+++ DoWhileCuti03_2.java
@@ -0,0 +1,36 @@
+package Jobsheet7;
+
+import java.util.Scanner;
+
+public class DoWhileCuti03_2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int jatahCuti, jumlahHari;
+        String konfirmasi;
+
+        System.out.print("Jatah cuti: ");
+        jatahCuti = sc.nextInt();
+
+        do{
+            System.out.print("Apakah Anda ingin mengambil cuti (y/t)? ");
+            konfirmasi = sc.next();
+
+            if (konfirmasi.equalsIgnoreCase("y")){
+                System.out.print("Jumlah hari: ");
+                jumlahHari = sc.nextInt();
+
+                if (jumlahHari <= jatahCuti){
+                    jatahCuti -= jumlahHari;
+                    System.out.println("Sisa jatah cuti: " + jatahCuti);
+                }else if (jatahCuti == 0){
+                    break;
+                }
+                else{
+                    System.out.println("Sisa jatah cuti anda tidak mencukupi. Sisa jatah cuti anda: " + jatahCuti);
+                }
+            }else {
+                break;
+            }
+        }while (jatahCuti > 0);
+    }
+}
